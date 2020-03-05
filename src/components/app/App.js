e033573225b865362957da2787006f36581901cb
Shpakova Alena <shpakovadev@gmail.com>
2020-03-05 22:02:40 +0300

    add spinner

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,11 @@
 import React from "react"
 import ErrorIndicator from "../error-indicator/error-indicator"
+import Spinner from "../spinner/spinner"
 
 function App() {
   return (
     <div className="App">
-      <ErrorIndicator />
+      <Spinner />
     </div>
   )
 }
---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,44 @@
+import React from "react"
+import "./spinner.scss"
+
+const Spinner = () => {
+  return (
+    <div className="spinner">
+      <div class="loadingio-spinner-spin-l8mbg6b2bhg">
+        <div class="ldio-a3uhalmf3m4">
+          <div>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Spinner
