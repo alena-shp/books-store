49e856c75af442c2b8294b8ecd7489c0810c67b7
Shpakova Alena <shpakovadev@gmail.com>
2020-03-06 15:39:47 +0300

    add router

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,15 @@
 import React from "react"
+import HomePage from "../pages/home-page/home-page"
+import CardPage from "../pages/card-page/card-page"
+import { Route, Switch } from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      App
+      <Switch>
+        <Route path="/" component={HomePage} exact />
+        <Route path="/cardpage" component={CardPage} />
+      </Switch>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { Provider } from "redux"
+import { Provider } from "react-redux"
 import { BrowserRouter as Router } from "react-router-dom"
 
 import "./index.css"
 
 import App from "./components/app/App"
-import bookService from "./services"
+import bookService from "./services/bookService"
 import { BookServiceProvider } from "./components/book-servise-context"
-import ErrorBoundary from "./components/error-boundary"
+import ErrorBoundary from "./components/error-boundary/error-boundary"
 import store from "./store"
 
 ReactDOM.render(
