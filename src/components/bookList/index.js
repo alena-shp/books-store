44a0b57cb6742f8dbdc505e57374f4a182122224
Shpakova Alena <shpakovadev@gmail.com>
2020-03-24 17:48:16 +0300

    fix componentDidMount

--- src/components/bookList/index.js
+++ src/components/bookList/index.js
@@ -3,13 +3,14 @@ import BookItem from "../bookItem"
 import { connect } from "react-redux"
 import { booksLoaded } from "./../../actions"
 import WithBookServiceContext from "../hoc/with-book-service-context"
-import './bookList.scss'
+import "./bookList.scss"
 
 class BookList extends React.Component {
   componentDidMount() {
     const { booksData } = this.props
-    const data = booksData.getBooks()
-    this.props.booksLoaded(data)
+    booksData.getBooks().then(data => {
+      this.props.booksLoaded(data)
+    })
   }
   render() {
     const { books } = this.props
