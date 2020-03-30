6832050875aa49fbd78122d7ece620314b46db32
Shpakova Alena <shpakovadev@gmail.com>
2020-03-30 19:00:47 +0300

    fix bookAddCart

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -19,14 +19,26 @@ const updateCartItems = (item, newItem, index) =>
     ? [...item, newItem]
     : [...item.slice(0, index), newItem, ...item.slice(index + 1)]
 
-const updateItem = (book, item = {}) => {
+const updateItem = (book, item = {}, amount) => {
   const { id = book.id, title = book.title, count = 0, total = 0 } = item
 
   return {
     id,
     title,
-    count: count + 1,
-    total: total + book.price
+    count: count + amount,
+    total: total + amount*book.price
+  }
+}
+
+const updateOrder = (state, bookId, amount) => {
+  const { books, cartItems } = state
+  const book = books.find(book => book.id === bookId)
+  const itemIndex = cartItems.findIndex(item => item.id === bookId)
+  const item = cartItems[itemIndex]
+  const newItem = updateItem(book, item, amount)
+  return {
+    ...state,
+    cartItems: updateCartItems(cartItems, newItem, itemIndex)
   }
 }
 
@@ -55,15 +67,7 @@ const reducer = (state = initialState, action) => {
       }
 
     case BOOK_ADD_CART:
-      const bookId = action.payload
-      const book = state.books.find(book => book.id === bookId)
-      const itemIndex = state.cartItems.findIndex(item => item.id === bookId)
-      const item = state.cartItems[itemIndex]
-      const newItem = updateItem(book, item)
-      return {
-        ...state,
-        cartItems: updateCartItems(state.cartItems, newItem, itemIndex)
-      }
+      return updateOrder(state, action.payload, 1)
 
     case BOOK_REMOVE_CART:
       const bookIdRemove = action.payload
